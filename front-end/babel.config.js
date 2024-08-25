c32cba97525b5538d3351e67ca5051e01d947429
aakashDev000 <123662760+aakashDev000@users.noreply.github.com>
2024-08-25 18:12:34 +0530

    Update babel.config.js

--- front-end/babel.config.js
+++ front-end/babel.config.js
@@ -1,5 +1,13 @@
-{
-    "presets": ["react-app"],
-    "plugins": ["@babel/plugin-proposal-private-property-in-object"]
-  }
-  
+// {
+//     "presets": ["react-app"],
+//     "plugins": ["@babel/plugin-proposal-private-property-in-object"]
+//   }
+
+module.exports = {
+  presets: ["react-app"],
+  env: {
+    production: {
+      plugins: ["@babel/plugin-proposal-private-property-in-object"],
+    },
+  },
+};
